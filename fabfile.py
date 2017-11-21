7747473984382da3e92df17f14f02e2d062b6522
Valentin Popescu <ppsc.valentin@gmail.com>
2017-11-21 17:36:22 +0200

    remove GA script from development version

--- fabfile.py
+++ fabfile.py
@@ -7,5 +7,5 @@ env['use_ssh_config'] = True
 
 def deploy():
     with cd('/var/www/html/dance.cloud/prod/docroot/dc/'):
-        run('git pull')
-        run('jekyll build')
+        run('git pull origin master')
+        run('JEKYLL_ENV=production jekyll build')
